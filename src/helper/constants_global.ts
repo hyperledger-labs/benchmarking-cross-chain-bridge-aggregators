f1f5aaa26442c88982ef22f49443d31c649782f0
shankars99 <shankars0899@gmail.com>
2023-08-13 10:31:52 -0400

    added ETH, GOERLI, MATIC to global support

--- src/helper/constants_global.ts
+++ src/helper/constants_global.ts
@@ -8,6 +8,7 @@ export const GOERLI: CHAIN = {
     chainId: 5,
     name: 'GOERLI',
     token_map: {
+        "ETH": "0x0000000000000000000000000000000000000000",
         "USDC": "0x07865c6E87B9F70255377e024ace6630C1Eaa37F",
         "DAI": "0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844",
         "WETH": "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
@@ -18,6 +19,7 @@ export const MAINNET: CHAIN = {
     chainId: 1,
     name: 'MAINNET',
     token_map: {
+        "ETH": "0x0000000000000000000000000000000000000000",
         "USDC": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
         "DAI": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
         "WETH": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
@@ -28,6 +30,7 @@ export const MATIC: CHAIN = {
     chainId: 137,
     name: 'MATIC',
     token_map: {
+        "MATIC": "0x0000000000000000000000000000000000000000",
         "USDC": "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
         "DAI": "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
         "WETH": "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
@@ -42,9 +45,9 @@ export const SUPPORTED_TOKENS: string[] = [
 ];
 
 export const SUPPORTED_CHAINS: { [key: string]: string[] } = {
-    "UNISWAP": ["MAINNET", "GOERLI"],
+    "LIFI": ["MAINNET", "GOERLI", "MATIC"],
     "SOCKET": ["MAINNET", "MATIC"],
-    "LIFI": ["MAINNET"],
+    "UNISWAP": ["MAINNET", "GOERLI", "MATIC"],
 }
 
 export const CHAIN_ID_MAP: { [key: number]: string } = {
--- src/helper/token_misc.ts
+++ src/helper/token_misc.ts
@@ -1,9 +1,9 @@
 import { ethers } from "ethers";
-import { getProvider, create_tx, send_tx } from "./provider";
+import { get_provider, create_tx, send_tx } from "./provider";
 
 export async function approve_token_transfer(contract_abi: string, token_address: string, spender_address: string, amount: string): Promise<ethers.providers.TransactionResponse> {
 
-    const tokenContract = new ethers.Contract(token_address, contract_abi, getProvider('GOERLI', 'ALCHEMY'));
+    const tokenContract = new ethers.Contract(token_address, contract_abi, get_provider('GOERLI', 'ALCHEMY'));
 
     const transaction = await tokenContract.approve(spender_address, amount);
 
