47aaafbb3fa7219bdad705843e01314a61452963
shankars99 <shankars0899@gmail.com>
2023-08-14 14:25:04 -0400

    TODO: route protocol apis for docker endpoint

---
+++ index.ts
@@ -0,0 +1,4 @@
+// Route the request to the protocols
+// This is the main entry point for the server run as a docker container
+
+console.log("pls run \nyarn run test:all\nthe server is not yet implemented");
