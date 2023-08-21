06e370ea9ea3f7ad8aa40d38631140607bc73179
shankars99 <shankars0899@gmail.com>
2023-08-21 18:31:29 -0400

    updated yarn scripts

--- test/message-aggregators/hyperlane/counter.test.sh
+++ test/message-aggregators/hyperlane/counter.test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
+echo "Testing Hyperlane"
 original_dir=$(pwd)
 
 cd "$(dirname "$0")"
 
 cd "$original_dir"
 source .env
-
+echo "Testing Contracts"
 forge test --match-path="test/contracts/Hyperlane/*"
+
+echo "Testing Scripts (dry-run)"
 forge script script/Hyperlane/Counter.s.sol --fork-url $ALCHEMY_RPC_GOERLI -vvvv
