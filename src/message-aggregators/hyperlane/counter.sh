2837d16b99c2c09eaba34a6e6ebc355cf7c9eb09
shankars99 <shankars0899@gmail.com>
2023-08-24 17:01:11 -0400

    updated test script with networks as input

--- src/message-aggregators/hyperlane/counter.sh
+++ src/message-aggregators/hyperlane/counter.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# Change directory to the location of the script
 original_dir=$(pwd)
-
 cd "$(dirname "$0")"
 
+# Load environment variables
 cd "$original_dir"
 source .env
 
+# Set source and destination domains
 export HYPERLANE_SOURCE_DOMAIN="$1"
 export HYPERLANE_DESTINATION_DOMAIN="$2"
 
-echo "Deploying the counter contract"
+# Deploy the counter contract
+echo "Deploying the counter contract on network: $HYPERLANE_SOURCE_DOMAIN"
 forge script script/Hyperlane/DeployCounter.s.sol:DeployCounterScript --rpc-url $ALCHEMY_RPC_GOERLI --broadcast -vvvv --verify
 
-echo "Sending the source transaction"
+# Send the source transaction
+echo "Sending the source transaction on network: $HYPERLANE_DESTINATION_DOMAIN"
 forge script script/Hyperlane/CounterSourceTx.s.sol:CounterSourceTxScript --rpc-url $ALCHEMY_RPC_MUMBAI --broadcast -vvvv
--- test/message-aggregators/hyperlane/counter.test.sh
+++ test/message-aggregators/hyperlane/counter.test.sh
@@ -7,8 +7,8 @@ cd "$(dirname "$0")"
 cd "$original_dir"
 source .env
 
-export HYPERLANE_SOURCE_DOMAIN="$1"
-export HYPERLANE_DESTINATION_DOMAIN="$2"
+export HYPERLANE_SOURCE_DOMAIN=$1
+export HYPERLANE_DESTINATION_DOMAIN=$2
 
 echo "Testing Contracts"
 
@@ -17,14 +17,13 @@ forge test --match-path="test/contracts/Hyperlane/*"
 echo "Sending the source transaction"
 echo "Testing Scripts (dry-run)"
 echo "Deploying the counter contract"
-forge script script/Hyperlane/DeployCounter.s.sol:DeployCounterScript --rpc-url $ALCHEMY_RPC_GOERLI -vvvv
 
+forge script script/Hyperlane/DeployCounter.s.sol:DeployCounterScript --rpc-url $ALCHEMY_RPC_GOERLI -vvvv
 forge script script/Hyperlane/CounterSourceTx.s.sol:CounterSourceTxScript --rpc-url $ALCHEMY_RPC_MUMBAI -vvvv
 
-Pay gas on the source chain for mainnet deployment
 
 echo "Goerli contract: https://goerli.etherscan.io/address/0x17c1bc8c169449228584b9c04b2472602e9423e1"
 
-echo "Note that the value of number has changed from 0 to 20"
-
 echo "Hyperlane explorer transaction: https://explorer.hyperlane.xyz/message/0xeea6da297e691ff37d1e5a90d4d62b33571d269c35476adf87407ea1e372dbbb"
+
+echo "Note that the value of number has changed from 0 to 20"
