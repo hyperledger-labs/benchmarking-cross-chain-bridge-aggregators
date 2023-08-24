e590d146e985560ee90f41142df26ba09ee595d6
shankars99 <shankars0899@gmail.com>
2023-08-24 12:42:33 -0400

    source and dest chain are user input

--- src/message-aggregators/hyperlane/counter.sh
+++ src/message-aggregators/hyperlane/counter.sh
@@ -7,6 +7,9 @@ cd "$(dirname "$0")"
 cd "$original_dir"
 source .env
 
+export HYPERLANE_SOURCE_DOMAIN="$1"
+export HYPERLANE_DESTINATION_DOMAIN="$2"
+
 echo "Deploying the counter contract"
 forge script script/Hyperlane/DeployCounter.s.sol:DeployCounterScript --rpc-url $ALCHEMY_RPC_GOERLI --broadcast -vvvv --verify
 
--- test/message-aggregators/hyperlane/counter.test.sh
+++ test/message-aggregators/hyperlane/counter.test.sh
@@ -6,17 +6,22 @@ cd "$(dirname "$0")"
 
 cd "$original_dir"
 source .env
+
+export HYPERLANE_SOURCE_DOMAIN="$1"
+export HYPERLANE_DESTINATION_DOMAIN="$2"
+
 echo "Testing Contracts"
+
 forge test --match-path="test/contracts/Hyperlane/*"
 
+echo "Sending the source transaction"
 echo "Testing Scripts (dry-run)"
 echo "Deploying the counter contract"
 forge script script/Hyperlane/DeployCounter.s.sol:DeployCounterScript --rpc-url $ALCHEMY_RPC_GOERLI -vvvv
 
-echo "Sending the source transaction"
 forge script script/Hyperlane/CounterSourceTx.s.sol:CounterSourceTxScript --rpc-url $ALCHEMY_RPC_MUMBAI -vvvv
 
-# Pay gas on the source chain for mainnet deployment
+Pay gas on the source chain for mainnet deployment
 
 echo "Goerli contract: https://goerli.etherscan.io/address/0x17c1bc8c169449228584b9c04b2472602e9423e1"
 
