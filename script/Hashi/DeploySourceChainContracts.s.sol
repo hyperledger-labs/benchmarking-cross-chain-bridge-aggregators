0ab3a3b61beec9016261e6e4f836fab6b6322d52
shankars99 <shankars0899@gmail.com>
2023-08-27 11:24:51 -0400

    deploys AMBRelay on source chain

--- script/Hashi/DeploySourceChainContracts.s.sol
+++ script/Hashi/DeploySourceChainContracts.s.sol
@@ -10,6 +10,10 @@ import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
 
 import {Yaho} from "@hashi/Yaho.sol";
+import {IAMB} from "@hashi/adapters/AMB/IAMB.sol";
+import {AMBMessageRelay} from "@hashi/adapters/AMB/AMBMessageRelayer.sol";
+
+import {HashiHelperScript} from "./HashiHelper.s.sol";
 
 contract DeployYahoScript is Script {
     uint256 deployerPrivateKey;
@@ -17,10 +21,10 @@ contract DeployYahoScript is Script {
     Yaho public yaho;
 
     uint256 SOURCE_DOMAIN;
-    uint256 DESTINATION_DOMAIN;
 
     function setUp() public {
         deployerPrivateKey = vm.envUint("KEY_PRIVATE");
+        SOURCE_DOMAIN = vm.envUint("HASHI_SOURCE_DOMAIN");
     }
 
     function run() public {
@@ -35,6 +39,51 @@ contract DeployYahoScript is Script {
             "Yaho deployed at address on chain %d: %s",
             SOURCE_DOMAIN,
             address(yaho)
+        );
+    }
+}
+
+contract DeployAMBRelayScript is Script {
+    HashiHelperScript hashiHelper;
+    uint256 deployerPrivateKey;
+
+    AMBMessageRelay public ambRelay;
+
+    uint256 SOURCE_DOMAIN;
+    string DESTINATION_DOMAIN;
+    address AMB_RELAY;
+    address DEPLOYED_YAHO;
+
+    function setUp() public {
+        hashiHelper = new HashiHelperScript();
+        deployerPrivateKey = vm.envUint("KEY_PRIVATE");
+
+        SOURCE_DOMAIN = vm.envUint("HASHI_SOURCE_DOMAIN");
+        DESTINATION_DOMAIN = vm.envString("HASHI_DESTINATION_DOMAIN");
+
+        AMB_RELAY = vm.envAddress("HASHI_AMB_RELAY_GOERLI");
+
+        bytes memory counterAddressBytes = hashiHelper.get_tx_data(
+            "DeploySourceChainContracts",
+            DESTINATION_DOMAIN,
+            ".transactions[0].contractAddress"
+        );
+
+        DEPLOYED_YAHO = abi.decode(counterAddressBytes, (address));
+    }
+
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy AMBMessageRelay on Source chain
+        ambRelay = new AMBMessageRelay(IAMB(AMB_RELAY), Yaho(DEPLOYED_YAHO));
+
+        vm.stopBroadcast();
+
+        console2.log(
+            "AMB Relay deployed at address on chain %d: %s",
+            SOURCE_DOMAIN,
+            address(ambRelay)
         );
     }
 }
