665951f84e47fda89c9ceaa34cef6554aee31ccb
shankars99 <shankars0899@gmail.com>
2023-09-06 09:07:30 -0400

    interacts with deployed contracts

---
+++ script/CCIP/CounterSourceTx.s.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
+
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+
+import {CCIP_Sender} from "@benchmarking-cross-chain-bridges/CCIP/SourceContract.sol";
+
+contract CounterSourceTxPayLINKScript is Script {
+    CCIP_Sender source_contract;
+    LinkTokenInterface linkToken;
+
+    uint64 DESTINATION_DOMAIN;
+    address SOURCE_CONTRACT_ADDRESS;
+    address DESTINATION_CONTRACT_ADDRESS;
+    address LINK_ADDRESS;
+
+    uint256 number;
+
+    uint256 deployerPrivateKey;
+
+    function setUp() public {
+        deployerPrivateKey = vm.envUint("KEY_PRIVATE");
+
+        SOURCE_CONTRACT_ADDRESS = vm.envAddress("CCIP_SENDER_ADDRESS");
+
+        DESTINATION_CONTRACT_ADDRESS = vm.envAddress("CCIP_RECEIVER_ADDRESS");
+
+        DESTINATION_DOMAIN = uint64(vm.envUint("CCIP_DESTINATION_DOMAIN"));
+
+        LINK_ADDRESS = vm.envAddress("CCIP_LINK_ADDRESS");
+
+        number = vm.envUint("CCIP_NUMBER");
+
+        source_contract = CCIP_Sender(payable(SOURCE_CONTRACT_ADDRESS));
+        linkToken = LinkTokenInterface(LINK_ADDRESS);
+    }
+
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+
+        linkToken.transfer(SOURCE_CONTRACT_ADDRESS, 1e17);
+
+        source_contract.sendMessagePayLINK(
+            DESTINATION_DOMAIN,
+            DESTINATION_CONTRACT_ADDRESS,
+            number
+        );
+
+        vm.stopBroadcast();
+    }
+}
+
+contract CounterSourceTxPayNativeScript is Script {
+    CCIP_Sender source_contract;
+
+    uint64 DESTINATION_DOMAIN;
+    address SOURCE_CONTRACT_ADDRESS;
+    address DESTINATION_CONTRACT_ADDRESS;
+
+    uint256 number;
+
+    uint256 deployerPrivateKey;
+
+    function setUp() public {
+        deployerPrivateKey = vm.envUint("KEY_PRIVATE");
+
+        SOURCE_CONTRACT_ADDRESS = vm.envAddress("CCIP_SENDER_ADDRESS");
+
+        DESTINATION_CONTRACT_ADDRESS = vm.envAddress("CCIP_RECEIVER_ADDRESS");
+
+        DESTINATION_DOMAIN = uint64(vm.envUint("CCIP_DESTINATION_DOMAIN"));
+
+        number = vm.envUint("CCIP_NUMBER");
+
+        source_contract = CCIP_Sender(payable(SOURCE_CONTRACT_ADDRESS));
+    }
+
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+
+        payable(SOURCE_CONTRACT_ADDRESS).transfer(1e16);
+
+        source_contract.sendMessagePayNative(
+            DESTINATION_DOMAIN,
+            DESTINATION_CONTRACT_ADDRESS,
+            number
+        );
+
+        vm.stopBroadcast();
+    }
+}
