4c5f2c4291d4ccb0ec9154a6b21d8363a39160c9
shankars99 <shankars0899@gmail.com>
2023-09-10 15:34:45 -0400

    refactor: move common validator scripts to inp_validator

--- src/helper/inp_validator.ts
+++ src/helper/inp_validator.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -76,4 +78,56 @@ export function validate_rpc_url(network: string): string {
     }
 
     return rpc_url;
+}
+
+export function _get_contract_address(chain_id: number, contract_name: string, isToken: boolean): string {
+
+    const chain_name = CHAIN_ID_MAP[chain_id];
+
+    if (chain_name in CHAIN_MAP) {
+        const chain = CHAIN_MAP[chain_name];
+        if (isToken) {
+            if (contract_name in chain.token_map) {
+                return chain.token_map[contract_name];
+            }
+            else {
+                throw new Error(`Token ${contract_name} not found in token_map`);
+            }
+        }
+        else {
+            if (contract_name in chain.address_map) {
+                return chain.address_map[contract_name];
+            }
+            else {
+                throw new Error(`Contract ${contract_name} not found in address_map`);
+            }
+        }
+    }
+
+    throw new Error(`Chain ${chain_name} not found in CHAIN_MAP`);
+}
+
+export function _get_contract_file_name(protocol_name: string, contract_map: { [key: string]: string }, contract_name: string): string[] {
+    const dispatcher_path = `src/message-aggregators/${protocol_name}/dispatcher.sh`;
+    if (contract_name in contract_map) {
+        return [dispatcher_path, contract_map[contract_name]];
+    }
+    else {
+        throw new Error(`Contract ${contract_name} not found in contract_map`);
+    }
+}
+
+export function _get_deployed_contract_address(protocol_name: string, contract_name: string): string {
+    const root_path = `src/message-aggregators/${protocol_name}`;
+    const dispatcher_path = `${root_path}/dispatcher.sh`;
+    const deployed_address_path = `${root_path}/deployed_addresses.json`;
+
+    const json = JSON.parse(fs.readFileSync(deployed_address_path).toString());
+
+    if (contract_name in json) {
+        return json[contract_name];
+    }
+    else {
+        throw new Error(`Contract ${contract_name} not found in deployed_address.json`);
+    }
 }
