507ddf30e7f8e6546a3910669692a91b59320bc7
shankars99 <shankars0899@gmail.com>
2023-09-25 09:29:46 -0400

    feat: custom types for cowprotocol

--- src/token-aggregators/cowswap/types.ts
+++ src/token-aggregators/cowswap/types.ts
@@ -1,3 +1,33 @@
+import { Order } from "@gnosis.pm/gp-v2-contracts";
+import { BigNumberish } from "ethers";
+
+export type CoWReturn = {
+    resp: Quote,
+    orderReq: OrderRequest,
+    order: Order
+}
+
+export type SignOrder = {
+    signingScheme: string,
+    signature: string,
+    publicKey: string
+}
+
+export type OrderRequest = {
+    sellToken: string,
+    buyToken: string,
+    receiver: string,
+    validTo: number,
+    appData: string,
+    partiallyFillable: boolean,
+    sellTokenBalance: string,
+    buyTokenBalance: string,
+    from: string,
+    kind: "sell" | "buy",
+    sellAmountBeforeFee: string,
+    buyAmountAfterFee: string,
+};
+
 export type Quote = {
     quote: {
         sellToken: string;
@@ -8,7 +38,7 @@ export type Quote = {
         validTo: number;
         appData: string;
         feeAmount: string;
-        kind: "sell" | "buy";
+        kind: "sell" | "buy"; // Adjust the possible values as needed
         partiallyFillable: boolean;
         sellTokenBalance: "erc20";
         buyTokenBalance: "erc20";
@@ -17,4 +47,40 @@ export type Quote = {
     from: string;
     expiration: string;
     id: number;
+};
+
+export type SignedOrder = {
+    sellToken: string,
+    buyToken: string,
+    sellAmount: number,
+    buyAmount: number,
+    receiver: string,
+    validTo: number,
+    appData: string,
+    feeAmount: string,
+    kind: "sell" | "buy",
+    partiallyFillable: boolean,
+    sellTokenBalance: "erc20",
+    buyTokenBalance: "erc20",
+    signingScheme: "ethsign",
+    signature: string,
+    from: string
+}
+
+export type CreateOrder = {
+    sellToken: string;
+    buyToken: string;
+    sellAmount: BigNumberish;
+    buyAmount: BigNumberish;
+    receiver: string;
+    validTo: number;
+    appData: string;
+    feeAmount: BigNumberish;
+    kind: string; // Assuming kind can only be "sell" or "buy"
+    partiallyFillable: boolean;
+    sellTokenBalance: string; // Assuming it can only be "erc20"
+    buyTokenBalance: string;  // Assuming it can only be "erc20"
+    signingScheme: string; // Assuming it can only be "ethsign"
+    signature: string; // Replace with the correct type for your signature
+    from: string;
 };
