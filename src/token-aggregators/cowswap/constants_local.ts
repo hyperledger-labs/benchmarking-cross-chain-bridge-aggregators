27596fb1714505a22f953b1e0c429a29a3b739bb
shankars99 <shankars0899@gmail.com>
2023-09-25 09:30:08 -0400

    refactor: helper functions into constants_local

--- src/token-aggregators/cowswap/constants_local.ts
+++ src/token-aggregators/cowswap/constants_local.ts
@@ -1,4 +1,6 @@
 import { CHAIN_MAP } from '@benchmarking-cross-chain-bridges/helper/constants_global';
+import { OrderBalance, OrderKind, Order } from '@gnosis.pm/gp-v2-contracts';
+import { OrderRequest, Quote } from './types';
 
 function create_tokens(chain_name: string): { [key: string]: string } {
     return CHAIN_MAP[chain_name].token_map;
@@ -9,3 +11,54 @@ export const TOKEN_MAP: { [key: number]: { [key: string]: string } } = {
     5: create_tokens('GOERLI'),
     100: create_tokens('GNOSIS'),
 };
+
+export function create_order(sourceChain: number, destChain: number, fromToken: string, toToken: string, amount: string, receiver: string, validTo: number, appData: string, sellTokenBalance: string, buyTokenBalance: string, from: string, kind: string): OrderRequest {
+    const fromToken_address = TOKEN_MAP[sourceChain][fromToken];
+    const toToken_address = TOKEN_MAP[destChain][toToken];
+
+    var sellAmountBeforeFee = amount;
+    var buyAmountAfterFee = '0';
+    if (kind === 'buy') {
+        sellAmountBeforeFee = '0';
+        buyAmountAfterFee = amount;
+    }
+    const orderRequest: OrderRequest = {
+        sellToken: fromToken_address,
+        buyToken: toToken_address,
+        receiver: receiver,
+        validTo: validTo,
+        appData: appData,
+        partiallyFillable: false,
+        sellTokenBalance: sellTokenBalance as OrderBalance,
+        buyTokenBalance: buyTokenBalance as OrderBalance,
+        kind: kind as OrderKind,
+        from: from,
+        sellAmountBeforeFee: sellAmountBeforeFee,
+        buyAmountAfterFee: buyAmountAfterFee,
+    }
+
+    return orderRequest;
+}
+
+export function get_order_from_quote(quote: Quote): Order {
+    const quote_obj = quote.quote;
+
+    let order_kind = OrderKind.SELL;
+
+    if (quote_obj.kind == 'buy') {
+        order_kind = OrderKind.BUY;
+    }
+
+    return {
+        sellToken: quote_obj.sellToken,
+        buyToken: quote_obj.buyToken,
+        sellAmount: quote_obj.sellAmount,
+        buyAmount: quote_obj.buyAmount,
+        validTo: quote_obj.validTo,
+        appData: quote_obj.appData,
+        feeAmount: quote_obj.feeAmount,
+        kind: order_kind,
+        partiallyFillable: quote_obj.partiallyFillable,
+        receiver: quote_obj.receiver
+    }
+}
