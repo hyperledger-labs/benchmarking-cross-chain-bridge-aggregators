075794e1469f67a62ab67b433f71fc7bb88c1832
shankars99 <shankars0899@gmail.com>
2023-11-22 17:24:59 -0500

    test: skip XY router execute_route
    Signed-off-by: shankars99 <shankars0899@gmail.com>

--- test/token-aggregators/xy/execute_route.test.ts
+++ test/token-aggregators/xy/execute_route.test.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { build_route } from '@benchmarking-cross-chain-bridges/token-aggregators/xy/route_builder';
 import { submit_order } from '@benchmarking-cross-chain-bridges/token-aggregators/xy/execute_route';
 
-describe('XY:Router', () => {
+describe.skip('XY:Router', () => {
     it('should return a quote for a ETHEREUM WETH to ETHEREUM USDC swap', async (done) => {
         const fromChain = 1;
         const fromToken = 'WETH';
