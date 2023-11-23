fbeb0a30677d96e59ee7a89c08f8385782b7a9d0
shankars99 <shankars0899@gmail.com>
2023-11-23 11:05:13 -0500

    feat: mainnet to mainnet ETH to USDC benchmark added
    Signed-off-by: shankars99 <shankars0899@gmail.com>

--- src/benchmark-runner/lifi.benchmark.ts
+++ src/benchmark-runner/lifi.benchmark.ts
@@ -16,10 +16,15 @@ describe('should create reports for GOERLI -> GOERLI trades', () => {
 
         expect(report).to.have.property('run_id');
     });
+});
 
-    it('should create an order for USDC to ETH swap', async () => {
-        const fromToken = 'USDC';
-        const toToken = 'ETH';
+describe('should create reports for ETHEREUM -> ETHEREUM trades', () => {
+    const fromChain = 1;
+    const toChain = 1;
+
+    it('should create an order for WETH to USDC swap', async () => {
+        const fromToken = 'ETH';
+        const toToken = 'USDC';
         const fromAmount = (1 * 10 ** 18).toString();
 
         const report: APIReport = await make_api_report(fromChain, toChain, fromToken, toToken, fromAmount);
@@ -28,12 +33,12 @@ describe('should create reports for GOERLI -> GOERLI trades', () => {
     });
 });
 
-describe('should create reports for MAINNET -> POLYGON trades', () => {
+describe('should create reports for ETHEREUM -> POLYGON trades', () => {
     const fromChain = 1;
     const toChain = 137;
 
     it('should create an order for ETH to MATIC swap', async () => {
-        const fromToken = 'ETH';
+        const fromToken = 'WETH';
         const toToken = 'MATIC';
         const fromAmount = (1 * 10 ** 18).toString();
 
