b8c1a9189f8425257c27ed3af69bcbd0c8c6df56
shankars99 <shankars0899@gmail.com>
2023-11-26 11:30:54 -0500

    todo: config for same chain transactions
    Signed-off-by: shankars99 <shankars0899@gmail.com>

---
+++ src/benchmark/runners/token-aggregators/xy.benchmark.ts
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+
+import { APIReport } from '@benchmarking-cross-chain-bridges/benchmark/types/APIReport';
+import { make_api_report } from '@benchmarking-cross-chain-bridges/benchmark/report-gen/token-aggregators/xy';
+
+describe('Benchmarking XY (get_token_price() may error if the coingecko pair is not correct)', () => {
+    describe.skip('should create reports for MAINNET ETHEREUM -> ETHEREUM trades', () => {
+        const sourceChain = 1;
+        const destChain = 1;
+
+        it('should benchmark WETH to USDC swap', async () => {
+            const fromToken = 'WETH';
+            const toToken = 'USDC';
+            const amount = (1 * 10 ** 18).toString();
+
+            const report: APIReport = await make_api_report(sourceChain, destChain, fromToken, toToken, amount);
+
+            expect(report).to.have.property('run_id');
+        });
+    });
+
+    describe('should create reports for ETHEREUM MAINNET -> POLYGON trades', () => {
+        const sourceChain = 1;
+        const destChain = 137;
+
+        it('should benchmark WETH to USDC swap', async () => {
+            const fromToken = 'WETH';
+            const toToken = 'USDC';
+            const amount = (1 * 10 ** 18).toString();
+
+            const report: APIReport = await make_api_report(sourceChain, destChain, fromToken, toToken, amount);
+
+            expect(report).to.have.property('run_id');
+        });
+    });
+});
