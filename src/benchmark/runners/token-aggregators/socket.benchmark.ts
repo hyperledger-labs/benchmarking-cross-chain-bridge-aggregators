5fd9a3e64654eff2d291fc71c1172bf67c812f75
shankars99 <shankars0899@gmail.com>
2023-12-08 11:42:12 -0500

    chore: ETH -> MATIC
    Signed-off-by: shankars99 <shankars0899@gmail.com>

--- src/benchmark/runners/token-aggregators/socket.benchmark.ts
+++ src/benchmark/runners/token-aggregators/socket.benchmark.ts
@@ -10,9 +10,19 @@ describe('Benchmarking Socket', () => {
         const fromChain = 1;
         const toChain = 137;
 
-        it('should create an order for ETH to USDC swap', async () => {
+        it('should create an order for WETH to USDC swap', async () => {
             const fromToken = 'WETH';
             const toToken = 'USDC';
+            const fromAmount = (1 * 10 ** 18).toString();
+
+            const report: APIReport = await make_api_report(fromChain, toChain, fromToken, toToken, fromAmount, multiTx);
+
+            expect(report).to.have.property('run_id');
+        });
+
+        it('should create an order for WETH to MATIC swap', async () => {
+            const fromToken = 'WETH';
+            const toToken = 'MATIC';
             const fromAmount = (1 * 10 ** 18).toString();
 
             const report: APIReport = await make_api_report(fromChain, toChain, fromToken, toToken, fromAmount, multiTx);
