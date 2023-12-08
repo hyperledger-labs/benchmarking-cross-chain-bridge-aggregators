0501dad02ea45545742469856b6505dd669a0802
shankars99 <shankars0899@gmail.com>
2023-12-08 06:17:41 -0500

    chore: generates plots for all source and destination networks for all protocols
    Signed-off-by: shankars99 <shankars0899@gmail.com>

--- src/benchmark/plotters/plot_runner.py
+++ src/benchmark/plotters/plot_runner.py
@@ -18,22 +18,31 @@ def plot_runner(benchmark_data_folder, aggregator, source_chain, dest_chain):
 
     plot_net_fee_vs_gas_price(timestamps_list, gas_prices_list, net_fees_list, aggregator, source_chain, dest_chain)
 
+def get_chain_names(arg_value, benchmark_data_folder, ignore_folders, path_modifier=''):
+    if arg_value == 'all':
+        # read the folder names in the benchmark-data folder
+        chain_names = os.listdir(benchmark_data_folder + path_modifier)
+        # remove the folders that are from the ignore list
+        chain_names = [name for name in chain_names if name not in ignore_folders]
+    else:
+        chain_names = [arg_value]
+
+    return chain_names
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--aggregator', default='all')
-parser.add_argument('--source_chain', required=True)
-parser.add_argument('--dest_chain', required=True)
+parser.add_argument('--source_chain', default='all')
+parser.add_argument('--dest_chain', default='all')
 args = parser.parse_args()
 
 benchmark_data_folder = 'benchmark-data/'
-ignore_folders = ['coin_gecko_price', 'logs']
+ignore_folders = ['coin_gecko_price', 'logs', 'uniswap-swap']
 
-if args.aggregator == 'all':
-    # read the folder names in the benchmark-data folder
-    aggregator_names = os.listdir(benchmark_data_folder)
-    # remove the folders that are from the ignore list
-    aggregator_names = [aggregator_name for aggregator_name in aggregator_names if aggregator_name not in ignore_folders]
+aggregator_names = get_chain_names(args.aggregator, benchmark_data_folder, ignore_folders, '')
 
-    for aggregator_name in aggregator_names:
-        plot_runner(benchmark_data_folder, aggregator_name, args.source_chain, args.dest_chain)
-else:
-    plot_runner(benchmark_data_folder, args.aggregator, args.source_chain, args.dest_chain)
+for aggregator in aggregator_names:
+    source_chain_names = get_chain_names(args.source_chain, benchmark_data_folder, ignore_folders, aggregator)
+    for source_chain in source_chain_names:
+        dest_chain_names = get_chain_names(args.dest_chain, benchmark_data_folder, ignore_folders, aggregator + '/' + source_chain)
+        for dest_chain in dest_chain_names:
+            plot_runner(benchmark_data_folder, aggregator, source_chain, dest_chain)
