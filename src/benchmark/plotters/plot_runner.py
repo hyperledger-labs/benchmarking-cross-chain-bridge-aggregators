87080e3a09ce925c33bde5def59e125bd70e7c04
shankars99 <shankars0899@gmail.com>
2023-12-12 08:04:49 -0500

    feat: tables the avergae latency when price difference is over, under, and overall
    Signed-off-by: shankars99 <shankars0899@gmail.com>

--- src/benchmark/plotters/plot_runner.py
+++ src/benchmark/plotters/plot_runner.py
@@ -1,9 +1,12 @@
 import os
 import argparse
+import numpy as np
 
-from data_loader import load_json_data
+from data_loader import load_json_data, convert_pd_to_csv
 from plot_feeVsgas import plot_net_fee_vs_gas_price
 from plot_quoteVscoingecko import plot_quote_vs_coingecko
+from plot_diff_in_quote import plot_diff_in_quotes
+from table_average_latency import table_average_latency
 
 def plot_runner(benchmark_data_folder, aggregator, source_chain, dest_chain):
     obj = load_json_data(benchmark_data_folder, aggregator, source_chain, dest_chain)
@@ -14,10 +17,17 @@ def plot_runner(benchmark_data_folder, aggregator, source_chain, dest_chain):
     source_gas_prices_list = obj['source_gas_prices']
     dest_gas_prices_list = obj['dest_gas_prices']
     effective_trade_value_usd_list = obj['effective_trade_value_usd']
+    latency_list = obj['latency']
 
     plot_quote_vs_coingecko(timestamps_list, coin_gecko_prices_list, effective_trade_value_usd_list, aggregator, source_chain, dest_chain)
 
     plot_net_fee_vs_gas_price(timestamps_list, source_gas_prices_list, dest_gas_prices_list,total_fees_list, aggregator, source_chain, dest_chain)
+
+    plot_diff_in_quotes(timestamps_list, coin_gecko_prices_list, effective_trade_value_usd_list, aggregator, source_chain, dest_chain)
+
+    latency_table = table_average_latency(latency_list, coin_gecko_prices_list, effective_trade_value_usd_list, aggregator, source_chain, dest_chain)
+
+    return latency_table
 
 def get_chain_names(arg_value, benchmark_data_folder, ignore_folders, path_modifier=''):
     if arg_value == 'all':
@@ -41,9 +51,15 @@ ignore_folders = ['coin_gecko_price', 'logs', 'uniswap-swap']
 
 aggregator_names = get_chain_names(args.aggregator, benchmark_data_folder, ignore_folders, '')
 
+latency_table = None
+
 for aggregator in aggregator_names:
     source_chain_names = get_chain_names(args.source_chain, benchmark_data_folder, ignore_folders, aggregator)
     for source_chain in source_chain_names:
         dest_chain_names = get_chain_names(args.dest_chain, benchmark_data_folder, ignore_folders, aggregator + '/' + source_chain)
         for dest_chain in dest_chain_names:
-            plot_runner(benchmark_data_folder, aggregator, source_chain, dest_chain)
+            latency_table = plot_runner(benchmark_data_folder, aggregator, source_chain, dest_chain)
+
+print(latency_table)
+table_path = 'benchmark-tables/'
+convert_pd_to_csv(latency_table, table_path + 'latency_table.csv')
