ba96c06a337223b894b187ad1e9acf34f328bdf5
shankars99 <shankars0899@gmail.com>
2023-12-15 06:05:03 -0500

    chore: Add support for saving plots as PNG and PDF files

--- src/benchmark/result/utils.py
+++ src/benchmark/result/utils.py
@@ -4,5 +4,9 @@ def create_plot_dir(fig, plot_dir, plot_filename):
     os.makedirs(plot_dir, exist_ok=True)
 
     plot_filename = f'{plot_dir}/{plot_filename}'
-    fig.write_image(plot_filename)
+
+    fig.write_image(plot_filename + '.png', format='png')
     print(f'Plot saved to: {plot_dir}/{plot_filename}')
+
+    fig.write_image(plot_filename + '.pdf', format='pdf')
+    print(f'PDF Plot saved to: {plot_filename}')
